35af45f754da3cbf720920d4aa6adc25aaa1dc57
fin10 <fender64@gmail.com>
2017-11-24 22:42:31 +0900

    Clear deprecated classes

--- neural_pos_tagger.py
+++ neural_pos_tagger.py
@@ -3,6 +3,8 @@ import random
 import numpy as np
 import tensorflow as tf
 
+tf.logging.set_verbosity(tf.logging.INFO)
+
 
 class NeuralPosTagger:
     __params = {}
@@ -18,25 +20,24 @@ class NeuralPosTagger:
             'learning_rate': 0.001,
         })
 
-        self.__estimator = tf.contrib.learn.Estimator(
+        self.__estimator = tf.estimator.Estimator(
             model_fn=self.__model_fn,
-            params=self.__params,
-            config=tf.contrib.learn.RunConfig(
-                save_checkpoints_steps=10,
+            model_dir=model_dir,
+            config=tf.estimator.RunConfig(
                 save_summary_steps=10,
+                save_checkpoints_steps=10,
             ),
-            model_dir=model_dir
+            params=self.__params,
         )
 
     @classmethod
     def __input_fn(cls, inputs, batch_size=-1, shuffle=False):
         features = {
-            'x': [],
+            'ids': [],
             'lengths': [],
             'masks': []
         }
-
-        targets = []
+        labels = []
 
         if shuffle:
             random.shuffle(inputs)
@@ -46,30 +47,28 @@ class NeuralPosTagger:
 
         max_length = cls.__params['max_length']
         for i in inputs:
-            features['x'].append(i['x'])
+            features['ids'].append(i['x'])
             features['lengths'].append(i['length'])
             features['masks'].append([1.0 if n < i['length'] else 0.0 for n in range(max_length)])
-            targets.append(i['y'])
+            labels.append(i['y'])
 
-        features['x'] = tf.constant(np.asarray(features['x']))
+        features['ids'] = tf.constant(np.asarray(features['ids']))
         features['lengths'] = tf.constant(features['lengths'])
         features['masks'] = tf.constant(features['masks'])
-        targets = tf.constant(np.asarray(targets))
+        labels = tf.constant(np.asarray(labels))
 
-        return features, targets
+        return features, labels
 
     @staticmethod
-    def __model_fn(features, target, mode, params):
+    def __model_fn(features, labels, mode, params):
         cell_size = params['cell_size']
         output_size = params['output_size']
-
         vocab_size = params['vocab_size']
         embedding_size = params['char_embedding_size']
-
         learning_rate = params['learning_rate']
         keep_prob = 1.0 if mode != tf.contrib.learn.ModeKeys.TRAIN else 0.5
 
-        x = features['x']
+        ids = features['ids']
         lengths = features['lengths']
         masks = features['masks']
 
@@ -79,22 +78,23 @@ class NeuralPosTagger:
             initializer=tf.random_uniform_initializer(-1, 1)
         )
 
-        inputs = tf.nn.embedding_lookup(char_embeddings, x)
+        inputs = tf.nn.embedding_lookup(char_embeddings, ids)
 
         def rnn_cell(cell_size):
             cell = tf.contrib.rnn.GRUCell(cell_size)
             cell = tf.contrib.rnn.DropoutWrapper(cell, output_keep_prob=keep_prob)
             return cell
 
-        outputs, _ = tf.nn.bidirectional_dynamic_rnn(
-            cell_fw=rnn_cell(cell_size),
-            cell_bw=rnn_cell(cell_size),
+        outputs, _ = tf.nn.dynamic_rnn(
+            cell=rnn_cell(cell_size),
+            # cell_fw=rnn_cell(cell_size),
+            # cell_bw=rnn_cell(cell_size),
             inputs=inputs,
             sequence_length=lengths,
             dtype=tf.float32
         )
 
-        outputs = outputs[0] + outputs[1]
+        # outputs = outputs[0] + outputs[1]
 
         outputs = tf.contrib.layers.fully_connected(
             inputs=outputs,
@@ -105,8 +105,8 @@ class NeuralPosTagger:
 
         loss = None
         eval_metric_ops = None
-        if mode != tf.contrib.learn.ModeKeys.INFER:
-            onehot_labels = tf.one_hot(target, output_size, dtype=tf.float32)
+        if mode != tf.estimator.ModeKeys.PREDICT:
+            onehot_labels = tf.one_hot(labels, output_size, dtype=tf.float32)
             loss = tf.losses.softmax_cross_entropy(
                 onehot_labels=onehot_labels,
                 logits=outputs,
@@ -114,15 +114,15 @@ class NeuralPosTagger:
             )
 
             eval_metric_ops = {
-                'accuracy': tf.contrib.metrics.streaming_accuracy(
-                    labels=target,
+                'accuracy': tf.metrics.accuracy(
+                    labels=labels,
                     predictions=predictions,
                     weights=masks
                 )
             }
 
         train_op = None
-        if mode == tf.contrib.learn.ModeKeys.TRAIN:
+        if mode == tf.estimator.ModeKeys.TRAIN:
             learning_rate = tf.train.exponential_decay(
                 learning_rate=learning_rate,
                 global_step=tf.train.get_global_step(),
@@ -130,14 +130,12 @@ class NeuralPosTagger:
                 decay_rate=0.96
             )
 
-            train_op = tf.contrib.layers.optimize_loss(
+            train_op = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(
                 loss=loss,
                 global_step=tf.train.get_global_step(),
-                learning_rate=learning_rate,
-                optimizer='Adam'
             )
 
-        return tf.contrib.learn.ModelFnOps(
+        return tf.estimator.EstimatorSpec(
             mode=mode,
             predictions={
                 'predictions': predictions
@@ -153,26 +151,28 @@ class NeuralPosTagger:
         train_set = inputs[:pivot]
         dev_set = inputs[pivot:]
 
-        validation_metrics = {
-            'accuracy': tf.contrib.learn.MetricSpec(
-                metric_fn=tf.contrib.metrics.streaming_accuracy,
-                prediction_key='predictions',
-            )
-        }
+        class ValidationHook(tf.train.SessionRunHook):
+            def __init__(self, estimator, input_fn, dataset):
+                self.__every_n_steps = 10
+                self.__estimator = estimator
+                self.__input_fn = input_fn
+                self.__dataset = dataset
 
-        monitor = tf.contrib.learn.monitors.ValidationMonitor(
-            input_fn=lambda: self.__input_fn(dev_set),
-            eval_steps=1,
-            every_n_steps=10,
-            metrics=validation_metrics,
-            early_stopping_metric='loss',
-            early_stopping_metric_minimize=True,
-            early_stopping_rounds=100
-        )
+            def before_run(self, run_context):
+                graph = run_context.session.graph
+                return tf.train.SessionRunArgs(tf.train.get_global_step(graph))
 
-        self.__estimator.fit(
+            def after_run(self, run_context, run_values):
+                if run_values.results % self.__every_n_steps == 0:
+                    result = self.__estimator.evaluate(
+                        input_fn=lambda: self.__input_fn(self.__dataset),
+                        steps=1,
+                    )
+                    print(result)
+
+        self.__estimator.train(
             input_fn=lambda: self.__input_fn(train_set, batch_size=self.__params['batch_size'], shuffle=True),
-            monitors=[monitor],
+            hooks=[ValidationHook(self.__estimator, self.__input_fn, dev_set)],
         )
 
     def eval(self, inputs):
