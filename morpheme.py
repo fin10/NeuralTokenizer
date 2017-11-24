5b278c2839dbb2fc8ca5fc0421dcf9ebda46e252
fin10 <fender64@gmail.com>
2017-11-24 20:04:57 +0900

    Change logic to match tags

---
+++ morpheme.py
@@ -0,0 +1,108 @@
+import os
+import re
+
+
+class Morpheme:
+    __MORPHEME_PATTERN = re.compile('([^/]+)/([a-z]+)')
+
+    __SLASH_PATTERN = re.compile('(/)/([a-z]+)')
+    __PLUS_PATTERN = re.compile('(\+)/([a-z]+)')
+
+    __SLASH = '_slash_'
+    __PLUS = '_plus_'
+
+    __matcher = None
+
+    @classmethod
+    def __encode(cls, labels: str):
+        labels = cls.__SLASH_PATTERN.sub(cls.__SLASH + '/' + '\g<2>', labels)
+        labels = cls.__PLUS_PATTERN.sub(cls.__PLUS + '/' + '\g<2>', labels)
+        return labels
+
+    @classmethod
+    def __decode(cls, text: str):
+        text = text.replace(cls.__SLASH, '/')
+        text = text.replace(cls.__PLUS, '+')
+        return text
+
+    def __init__(self, raw: str):
+        self.__tags = []
+
+        raw = self.__encode(raw)
+        for chunk in raw.split('+'):
+            m = self.__MORPHEME_PATTERN.match(chunk)
+            text = m.group(1)
+            tag = m.group(2)
+
+            text = self.__decode(text)
+            for ch in text:
+                self.__tags.append((ch, tag))
+
+    def __len__(self) -> int:
+        return len(self.__tags)
+
+    def __str__(self):
+        return '+'.join(['%s/%s' % (text, tag) for text, tag in self.__tags])
+
+    def tags(self):
+        return self.__tags
+
+    def match(self, targets):
+        if Morpheme.__matcher is None:
+            Morpheme.__matcher = Morpheme.Matcher(
+                os.path.join(os.path.dirname(os.path.abspath(__file__)), './res/complex_morpheme.dict'))
+
+        tags = [None for _ in range(len(targets))]
+        remaining = list(self.__tags)
+        index = 0
+        for tag in self.__tags:
+            for i in range(index, len(targets)):
+                if tag[0].lower() == targets[i].lower():
+                    tags[i] = tag[1]
+                    remaining.remove(tag)
+                    index = i + 1
+                    break
+
+        if None in tags:
+            for i in range(len(tags)):
+                if tags[i] is None:
+                    tag, matches = Morpheme.__matcher.match(targets[i], remaining)
+                    if tag is None:
+                        raise ValueError('%s %s' % (targets, self))
+                    else:
+                        tags[i] = tag
+                        for match in matches:
+                            remaining.remove(match)
+        return tags
+
+    class Matcher:
+
+        def __init__(self, path):
+            self.__dict = {}
+            with open(path) as fp:
+                for line in fp:
+                    items = line.strip().split(' ')
+                    key = items[0]
+                    value = [ch for ch in items[1]]
+
+                    if key not in self.__dict:
+                        self.__dict[key] = []
+                    self.__dict[key].append(value)
+
+            for value in self.__dict.values():
+                value.sort(key=lambda s: len(s))
+                value.reverse()
+
+        def match(self, ch, tags):
+            patterns = self.__dict.get(ch)
+            if patterns:
+                for pattern in patterns:
+                    for i in range(len(tags)):
+                        if tags[i][0] == pattern[0] and len(tags) - i >= len(pattern):
+                            subset = tags[i:(i + len(pattern))]
+                            if pattern == [ch for ch, tag in subset]:
+                                return ''.join(tag for ch, tag in subset), subset
+            elif ch == '으':
+                return 'mg', []
+
+            return None, None
--- tests/test_morpheme.py
+++ tests/test_morpheme.py
@@ -4,20 +4,51 @@ from morpheme import Morpheme
 
 
 class MorphemeTest(unittest.TestCase):
+
     def test(self):
         test_case = '421/nb+년/nu+째/xn'
         morpheme = Morpheme(test_case)
-        self.assertEqual([ch for ch in '421년째'], morpheme.text())
-        self.assertEqual(['nb', 'nb', 'nb', 'nu', 'xn'], morpheme.tag())
+        self.assertEqual([ch for ch in '421년째'], [ch for ch, tag in morpheme.tags()])
+        self.assertEqual(['nb', 'nb', 'nb', 'nu', 'xn'], [tag for ch, tag in morpheme.tags()])
 
     def test_slash(self):
         test_case = 'TCP/ne+//sc+IP/ne+를/po'
         morpheme = Morpheme(test_case)
-        self.assertEqual([ch for ch in 'TCP/IP를'], morpheme.text())
-        self.assertEqual(['ne', 'ne', 'ne', 'sc', 'ne', 'ne', 'po'], morpheme.tag())
+        self.assertEqual([ch for ch in 'TCP/IP를'], [ch for ch, tag in morpheme.tags()])
+        self.assertEqual(['ne', 'ne', 'ne', 'sc', 'ne', 'ne', 'po'], [tag for ch, tag in morpheme.tags()])
 
     def test_plus(self):
         test_case = '(/sl++/so+)/sr+반파/nc'
         morpheme = Morpheme(test_case)
-        self.assertEqual([ch for ch in '(+)반파'], morpheme.text())
-        self.assertEqual(['sl', 'so', 'sr', 'nc', 'nc'], morpheme.tag())
+        self.assertEqual([ch for ch in '(+)반파'], [ch for ch, tag in morpheme.tags()])
+        self.assertEqual(['sl', 'so', 'sr', 'nc', 'nc'], [tag for ch, tag in morpheme.tags()])
+
+    def test_match(self):
+        test_case = ('썼거니와', '쓰/vb+었/ep+거니와/ec')
+        morpheme = Morpheme(test_case[1])
+        result = morpheme.match(test_case[0])
+        self.assertEqual(['vbep', 'ec', 'ec', 'ec'], result)
+
+    def test_match2(self):
+        test_case = ('끝내!', '끝내/vb+아/ef+!/sf')
+        morpheme = Morpheme(test_case[1])
+        result = morpheme.match(test_case[0])
+        self.assertEqual(['vb', 'vb', 'sf'], result)
+
+    def test_match3(self):
+        test_case = ('선각자였습니다.', '선/nc+각/nc+자/nc+이/pp+었/ep+습/ef+니/ef+다/ef+./sf')
+        morpheme = Morpheme(test_case[1])
+        result = morpheme.match(test_case[0])
+        self.assertEqual(['nc', 'nc', 'nc', 'ppep', 'ef', 'ef', 'ef', 'sf'], result)
+
+    def test_match4(self):
+        test_case = ('활용한다든가', '활/na+용/na+하/xv+ㄴ/ec+다/ec+든/ec+가/ec')
+        morpheme = Morpheme(test_case[1])
+        result = morpheme.match(test_case[0])
+        self.assertEqual(['na', 'na', 'xvec', 'ec', 'ec', 'ec'], result)
+
+    def test_match5(self):
+        test_case = ('보십시오.', '보/vx+시/ep+ㅂ/ef+시/ef+오/ef+./sf')
+        morpheme = Morpheme(test_case[1])
+        result = morpheme.match(test_case[0])
+        self.assertEqual(['vx', 'epef', 'ef', 'ef', 'sf'], result)
